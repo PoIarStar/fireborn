79a057020785502f791f6c3fcd5929ccaf2bc27a
PolarStar <107398572+PoIarStar@users.noreply.github.com>
2025-06-24 12:54:46 +0300

    Some reorganization. Impact and effects

---
+++ core/other/effects/effect.gd
@@ -0,0 +1,37 @@
+extends Node
+class_name Effect
+
+
+var lifetime: int
+var repeat_time: int
+
+
+func _init(lifetime: int, repeat_time: int) -> void:
+	self.lifetime = lifetime
+	self.repeat_time = repeat_time
+	applying()
+	$LifetimeTimer.start(lifetime)
+	$RepeatTimer.start(repeat_time)
+
+
+func applying():
+	pass
+	
+	
+func process():
+	pass
+
+
+func removing():
+	pass
+
+
+
+func _on_lifetime_timer_timeout() -> void:
+	removing()
+	queue_free()  # delete object
+
+
+func _on_repeat_timer_timeout() -> void:
+	process()
+	$RepeatTimer.start(repeat_time)
---
+++ core/structures/impact.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name Impact
+
+var damage: int
+var effects: Array[Effect]
